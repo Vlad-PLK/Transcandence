bea83f93994ddb7888de89976d1b50d110a734fc
l1mpoln <kuzminvova342@gmail.com>
2024-08-15 05:14:21 +0200

    matches add and show added

--- backend/friends/views.py
+++ backend/friends/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
+
 class FriendRequestCreateView(generics.CreateAPIView):
     serializer_class = FriendRequestSerializer
     permission_classes = [IsAuthenticated]
--- backend/gameinfo/views.py
+++ backend/gameinfo/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
-from .models import PlayerStats
-from .serializers import PlayerStatsSerializer, PlayerInfoSerializer
+from .models import PlayerStats, Match
+from .serializers import PlayerStatsSerializer, PlayerInfoSerializer, MatchSerializer
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
@@ -26,4 +26,25 @@ class PlayerInfoView(APIView):
     def get(self, request):
         user = request.user
         serializer = PlayerInfoSerializer(user)
+        return Response(serializer.data)
+
+
+class MatchCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = MatchSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class GetMatches(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        matches = Match.objects.filter(player1=user) | Match.objects.filter(player2=user)
+        serializer = MatchSerializer(matches, many=True)
         return Response(serializer.data)
