ff5ee45701cada8ff5b7a7c0afe61ddf72ac3e3a
TVincil <thomasvinci@monaco.mc>
2024-09-09 15:32:22 +0200

    game context

--- front-react/src/GameSettingsModal.jsx
+++ front-react/src/GameSettingsModal.jsx
@@ -1,20 +1,35 @@
-function GameSettingsModal() {
+import React, { useState } from 'react';
+
+function GameSettingsModal({ onStarTypeChange }) {
+    const [starType, setstarType] = useState(0);  // Default size is 1.5
+
+    const handleStarTypeChange = (event) => {
+        console.log(event.target.value);
+        setstarType(event.target.value);
+    };
+
     return (
         <>
             <div className="modal fade" id="gameSettings" tabIndex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
-        	<div className="modal-dialog modal-dialog-centered modal-lg" role="document">
-        		<div className="modal-content rounded-4 shadow">
-        		  <div className="modal-header p-5 pb-4 border-bottom-0">
-        		    <h1 className="fw-bold mb-0 fs-4" id="loginModalLabel">Welcome back !</h1>
-        		    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        		  </div>
-        		  <div className="modal-body p-5 pt-0">
-        		  </div>
-        		</div>
-        	</div>
-		</div>
+                <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
+                    <div className="modal-content rounded-4 shadow">
+                        <div className="modal-header p-5 pb-4 border-bottom-0">
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body p-5 pt-0">
+                            <h4>What type of star</h4>
+                            <select className="form-select" value={starType} onChange={handleStarTypeChange}>
+                                <option value="0">The Sun</option>
+                                <option value="1">White Dwarf</option>
+                                <option value="2">Red Giant</option>
+                                <option value="3">Else</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
-    )
+    );
 }
 
-export default GameSettingsModal
+export default GameSettingsModal;
