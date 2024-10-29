0ebe03560fd2da05a138062ead1168294a1cd06e
Thomas Vincileoni <thomasvinci@monaco.mc>
2024-10-29 14:37:13 +0100

    new translations again

--- front-react/src/TwoFAModal.jsx
+++ front-react/src/TwoFAModal.jsx
@@ -67,10 +67,9 @@ function TwoFAModal({username, password})
                             <path d="M8 5a1 1 0 0 1 1 1v1H7V6a1 1 0 0 1 1-1m2 2.076V6a2 2 0 1 0-4 0v1.076c-.54.166-1 .597-1 1.224v2.4c0 .816.781 1.3 1.5 1.3h3c.719 0 1.5-.484 1.5-1.3V8.3c0-.627-.46-1.058-1-1.224M6.105 8.125A.64.64 0 0 1 6.5 8h3a.64.64 0 0 1 .395.125c.085.068.105.133.105.175v2.4c0 .042-.02.107-.105.175A.64.64 0 0 1 9.5 11h-3a.64.64 0 0 1-.395-.125C6.02 10.807 6 10.742 6 10.7V8.3c0-.042.02-.107.105-.175"/>
                             <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1"/>
                         </svg>
-                        <h1 className="fw-bold mb-2 mt-2 fs-4" id="loginModalLabel">Two Step verification</h1>
-                        <p className="mt-2">We sent a verification code to your email<br/>
-                        Please enter the code in the field below.</p>
-        		    </div>
+                        <h1 className="fw-bold mb-2 mt-2 fs-4" id="loginModalLabel">{t("2FA.two_step_verification")}</h1>
+                        <p className="mt-2">{t("2FA.verification_message")}</p>
+                    </div>
                     <div className="modal-body p-5 pt-0">
                     <div className="row pt-2">
                         {[...Array(6)].map((_, index) => (
@@ -88,7 +87,7 @@ function TwoFAModal({username, password})
                         ))}
                     </div>
                     <div className="d-flex flex-column align-items-center">
-                        <button className="btn btn-success btn-lg mt-4 align-items-center" data-bs-dismiss="modal" onClick={send_otp}>Verify</button>
+                    <button className="btn btn-success btn-lg mt-4 align-items-center" data-bs-dismiss="modal" onClick={send_otp}>{t("2FA.verify")}</button>
                         {errorLog && <p className="mt-2 text-danger">{errorLog}</p>}
                     </div>
         		  	</div>
